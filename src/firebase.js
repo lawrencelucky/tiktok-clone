8a681ba143513c3336f10d210493aac43ce67f2b
lawrencelucky <lawrencelucky1999@gmail.com>
2020-11-09 17:36:28 +0100

    Feat: Initialising the project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyACDNRb3Sd3Z4ddtjfNy21gUwNy7Nd-UZU',
+  authDomain: 'tiktok-clone-ba0f5.firebaseapp.com',
+  databaseURL: 'https://tiktok-clone-ba0f5.firebaseio.com',
+  projectId: 'tiktok-clone-ba0f5',
+  storageBucket: 'tiktok-clone-ba0f5.appspot.com',
+  messagingSenderId: '710991033970',
+  appId: '1:710991033970:web:1b06340ad4ff10930f74eb',
+  measurementId: 'G-WFRLZVZP6Z',
+};
