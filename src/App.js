2e77e87da3cc6134e2dd799d25cbf709fe2ea76c
lawrencelucky <lawrencelucky1999@gmail.com>
2020-11-10 14:00:31 +0100

    Feat: Adding the tiktok full project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
+import db from './firebase';
+
+import Video from './Video';
+
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    db.collection('video').onSnapshot(snapshot => {
+      setVideos(snapshot.docs.map(doc => doc.data()));
+    });
+  }, []);
+
   return (
-    <div className="app">
-      <h1>Tik Tok Clone</h1>
+    <div className='app'>
+      <div className='app__videos'>
+        {videos.map(
+          ({ url, description, channel, song, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              description={description}
+              song={song}
+              likes={likes}
+              messages={messages}
+              shares={shares}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyACDNRb3Sd3Z4ddtjfNy21gUwNy7Nd-UZU',
   authDomain: 'tiktok-clone-ba0f5.firebaseapp.com',
@@ -8,3 +10,9 @@ const firebaseConfig = {
   appId: '1:710991033970:web:1b06340ad4ff10930f74eb',
   measurementId: 'G-WFRLZVZP6Z',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
